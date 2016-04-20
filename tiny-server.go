95451498aeab5abbf8c1f9c460625f2b2d8933bc
Francis Norton <francis.norton@gmail.com>
2016-04-20 12:14:48 +0100

    Added a guid generator

--- tiny-server.go
+++ tiny-server.go
@@ -1,9 +1,12 @@
+// tiny-server generates dummy JSON responses
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
-  "log"
+	"io"
+	"log"
 	"net/http"
 )
 
@@ -13,20 +16,40 @@ type UserId struct {
 	IsVerified bool
 }
 
-var user = UserId{
-	UserId:     "4d27dee1-9630-49cb-bed1-c656e160fddd",
-	IsNewUser:  false,
-	IsVerified: true,
-}
-
 var userJson []byte
 
+// newUUID generates a random UUID according to RFC 4122
+func newUUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	// variant bits; see section 4.1.1
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-  userJson, err := json.Marshal(user)
+
+	uuid, err := newUUID()
+	if err != nil {
+		log.Fatalf("error: %v\n", err)
+	}
+
+	var user = UserId{
+		UserId:     uuid,
+		IsNewUser:  false,
+		IsVerified: true,
+	}
+
+	userJson, err := json.Marshal(user)
 	if err != nil {
 		log.Fatalf("JSON marshalling failed: %s", err)
 	}
-	fmt.Fprintf(w, "here's some json >>%s<<", userJson)
+	fmt.Fprintf(w, "%s", userJson)
 }
 
 func main() {
