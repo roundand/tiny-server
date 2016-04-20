7bd6e196a9b7be1e0d6db8c3a43615421274fbd6
Francis Norton <francis.norton@gmail.com>
2016-04-20 10:52:34 +0100

    Added JSON serialisation

--- tiny-server.go
+++ tiny-server.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"encoding/json"
+	"fmt"
+  "log"
+	"net/http"
 )
 
+type UserId struct {
+	UserId     string
+	IsNewUser  bool
+	IsVerified bool
+}
+
+var user = UserId{
+	UserId:     "4d27dee1-9630-49cb-bed1-c656e160fddd",
+	IsNewUser:  false,
+	IsVerified: true,
+}
+
+var userJson []byte
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+  userJson, err := json.Marshal(user)
+	if err != nil {
+		log.Fatalf("JSON marshalling failed: %s", err)
+	}
+	fmt.Fprintf(w, "here's some json >>%s<<", userJson)
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
